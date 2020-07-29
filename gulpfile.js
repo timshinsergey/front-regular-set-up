a41aed5878fa99cc6394597b92577bb3e8a2a3fa
Sergey Timshin <timshinsergey1@gmail.com>
2020-07-29 14:11:30 +0400

    major update

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,50 @@ const autoprefixer = require('gulp-autoprefixer');
 const notify = require('gulp-notify');
 const gcmq = require('gulp-group-css-media-queries');
 const imagemin = require('gulp-imagemin');
+const svgSprite = require('gulp-svg-sprite');
+
+const svgViewConfig = {
+    mode: {
+		view: {
+			transform: ['svgo'],
+			svg: { // General options for created SVG files
+				xmlDeclaration: false, // Add XML declaration to SVG sprite
+				doctypeDeclaration: false, // Add DOCTYPE declaration to SVG sprite
+				namespaceIDs: true, // Add namespace token to all IDs in SVG shapes
+				namespaceIDPrefix: '', // Add a prefix to the automatically generated namespaceIDs
+				namespaceClassnames: true, // Add namespace token to all CSS class names in SVG shapes
+				dimensionAttributes: true, // Width and height attributes on the sprite
+			},
+			layout: 'vertical',
+			dest: './',
+			bust: false
+		}
+	}
+}
+
+const svgSymbolConfig = {
+    mode: {
+		symbol: {
+			transform: ['svgo'],
+			svg: { // General options for created SVG files
+				xmlDeclaration: false, // Add XML declaration to SVG sprite
+				doctypeDeclaration: false, // Add DOCTYPE declaration to SVG sprite
+				namespaceIDs: true, // Add namespace token to all IDs in SVG shapes
+				namespaceIDPrefix: '', // Add a prefix to the automatically generated namespaceIDs
+				namespaceClassnames: true, // Add namespace token to all CSS class names in SVG shapes
+				dimensionAttributes: true, // Width and height attributes on the sprite
+			},
+			dest: './',
+		}
+	}
+}
 
 const config = {
     paths: {
         base: './',
         entry: {
             js: {
-                main: 'src/scripts/main.js',
+                main: 'src/js/main.js',
                 libs: [
                     // слайдер
                     'src/vendors/swiper/swiper.min.js',
@@ -36,18 +73,20 @@ const config = {
                     'src/vendors/flatpickr/l10n/ru.js',
                 ],
             },
-            commonScss: 'src/scss/main.scss',
+            commonScss: 'src/scss/common.scss',
             scssByPage: 'src/scss/views/*',
-            imgs: 'src/images/**/*'
+            imgs: 'src/imgs/**/*',
+            svgSymbol: 'src/imgs/svg/symbol/*.svg',
+            svgView: 'src/imgs/svg/view/*.svg',
         },
         output: {
-            js: 'assets/scripts',
+            js: 'assets/js',
             css: 'assets/css',
-            imgs: 'assets/images'
+            imgs: 'assets/imgs',    
         },
         watch: {
             scss: 'src/scss/**/*.scss',
-            js: 'src/scripts/**/*.js',
+            js: 'src/js/**/*.js',
             html: '**/*.html',
         }
     }
@@ -65,6 +104,7 @@ gulp.task('browser-sync', function () {
     })
 })
 
+// компилим скрипты
 gulp.task('main-js', function () {
     return gulp
         .src(config.paths.entry.js.main)
@@ -72,7 +112,8 @@ gulp.task('main-js', function () {
 		.pipe(gulp.dest(config.paths.output.js))
 		.pipe(browserSync.stream())
 })
-//
+
+// компилим либы в один бандл жс
 gulp.task('libs-js', function () {
     return gulp
         .src(config.paths.entry.js.libs)
@@ -80,20 +121,22 @@ gulp.task('libs-js', function () {
         .pipe(uglify())
         .pipe(gulp.dest(config.paths.output.js))
 })
-// для сборки стилей сайта для всего сайта
-gulp.task('bundle-css', function () {
+
+// компилим общий css
+gulp.task('common-css', function () {
     return gulp
         .src(config.paths.entry.commonScss)
         .pipe(sass({ outputStyle: 'expanded' }).on('error', notify.onError()))
         .pipe(autoprefixer(['last 2 versions']))
         .pipe(gcmq())
         .pipe(cleanCSS())
-        .pipe(concat('main.css'))
         .pipe(gulp.dest(config.paths.output.css))
         .pipe(browserSync.stream())
 })
-// для сборки стилей постранично
+
+// компилим css для страниц
 // например: src/scss/views/index.scss - для главной страницы, выходной файл - index.css
+//			src/scss/views/product-page.scss - для страницы продукта, выходной файл - product-page.css и тд
 gulp.task('css-by-page', function () {
     return gulp
         .src(config.paths.entry.scssByPage)
@@ -105,22 +148,54 @@ gulp.task('css-by-page', function () {
         .pipe(browserSync.stream())
 })
 
+//сжимаем картинки
 gulp.task('imagemin', function () {
 	return gulp
 		.src(config.paths.entry.imgs)
 		.pipe(imagemin([
 			imagemin.mozjpeg({quality: 75, progressive: true}),
-			imagemin.optipng({optimizationLevel: 5})
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+				plugins: [
+					{removeViewBox: true},
+					{cleanupIDs: false}
+				]
+			})
 		]))
 		.pipe(gulp.dest(config.paths.output.imgs))
 })
 
-gulp.task('watch', function () {
-    gulp.watch(config.paths.watch.scss, gulp.series('bundle-css'))
-    gulp.watch(config.paths.watch.js, gulp.series('libs-js'))
-    gulp.watch(config.paths.watch.js, gulp.series('main-js'))
+// собираем свг иконки в один спрайт типа symbol
+// вставляется в html
+// пример вставки:
+// <svg fill="none" viewBox="0 0 18 18"><use xlink:href="templates/assets/img/svg/sprite.symbol.svg#cross" /></svg>
+gulp.task('svg-sprite-symbol', function () {
+	return gulp
+		.src(config.paths.entry.svgSymbol)
+		.pipe(svgSprite(svgSymbolConfig))
+		.pipe(gulp.dest(config.paths.output.imgs))
 })
 
-gulp.task('build', gulp.parallel('bundle-css', 'libs-js', 'main-js'))
+// собираем свг иконки в один спрайт типа view
+// вставляется в css
+// пример вставки:
+// background-image: url(templates/assets/img/svg/sprite.view.svg#cross);
+// либо background-image: url(templates/assets/img/svg/sprite.view.svg);
+//		background-position: 35% 40%; - при помощи смены позиции ищем нужную иконку
+gulp.task('svg-sprite-view', function () {
+	return gulp
+		.src(config.paths.entry.svgView)
+		.pipe(svgSprite(svgViewConfig))
+		.pipe(gulp.dest(config.paths.output.imgs))
+})
+
+//смотрим за изменением файлов и перекомпиливаем стили и скрипты
+gulp.task('watch', function () {
+    gulp.watch(config.paths.watch.scss, gulp.series('common-css', 'css-by-page'))
+    gulp.watch(config.paths.watch.js, gulp.series('libs-js', 'main-js'))
+})
+
+//все полностью собираем
+gulp.task('build', gulp.parallel('common-css', 'css-by-page', 'libs-js', 'main-js'))
 
 gulp.task('default', gulp.parallel('browser-sync', 'watch'))
---
+++ src/js/main.js
@@ -0,0 +1,164 @@
+window.addEventListener('DOMContentLoaded', function() {
+    initHamburger()
+    initModalFormWindow()
+    initAccordion()
+	initPhoneMask()
+	initValidationForm()
+	setTimeout(inputsClassToggle, 100)
+    
+    // кастомный дейтпикер
+    flatpickr('[data-date]', { 'locale': 'ru' })
+})
+
+function initValidationForm() {
+    let forms = document.querySelectorAll('form')
+    if (forms.length) {
+        forms.forEach(form => {
+            let pristine = new Pristine(form)
+            form.addEventListener('submit', function (e) {
+                e.preventDefault()
+                pristine.validate()
+            })
+        })
+    }
+}
+
+function initAccordion() {
+    let acc = document.querySelectorAll('.accordion-btn')
+    acc.forEach(btn => {
+        btn.addEventListener('click', () => {
+            if (btn.disabled) return
+
+            let alreadyOpened = btn.classList.contains('is-active')
+
+            acc.forEach(el => {
+                if (el.classList.contains('is-active')) {
+                    el.classList.remove('is-active')
+                    el.nextElementSibling.style.height = null
+                }
+            })
+
+            if (!alreadyOpened) {
+                btn.classList.add('is-active')
+
+                let panel = btn.nextElementSibling
+
+                if (panel.style.height) {
+                    panel.style.height = null;
+                } else {
+                    panel.style.height = panel.scrollHeight + "px";
+                }
+            }
+
+            btn.disabled = true
+            setTimeout(() => btn.disabled = false, 100)
+        })
+    })
+}
+
+function initHamburger() {
+	let hambs = document.querySelectorAll('.hamburger')
+
+	hambs.forEach(hamb => {
+		hamb.addEventListener('click', event => {
+			if (hamb.disabled) return
+	
+			if (document.body.classList.contains('-menu-open')) {
+				document.body.classList.remove('-menu-open')
+			} else {
+				document.body.classList.add('-menu-open')
+			}
+	
+			hamb.disabled = true
+			setTimeout(() => hamb.disabled = false, 100)
+		})
+	})
+}
+
+function initModalFormWindow() {
+    const btns = document.querySelectorAll('[data-modal]')
+    const closeBtns = document.querySelectorAll('.modal-close')
+    btns.forEach(btn => {
+        btn.addEventListener('click', event => {
+			modalOpen(btn.dataset.modal, btn.dataset.video)
+			
+			event.preventDefault()
+        })
+    })
+    closeBtns.forEach(close => {
+        close.addEventListener('click', event => {
+			let target = event.currentTarget
+
+			modalClose(`#${target.closest('.modal').id}`)
+        })
+    })
+}
+
+function modalOpen(modalSel, video) {
+	let modal = document.querySelector(modalSel)
+	let openedModals = document.querySelectorAll('.modal.-is-open')
+
+	if (openedModals.length) {
+		openedModals.forEach(openedModal => { modalClose(`#${openedModal.id}`) })
+	}
+
+	document.body.classList.add('-modal-open')
+	modal.classList.add('-is-open')
+	if (video != undefined) {
+		if (modal.querySelector('iframe').src.indexOf('youtube')) {
+			modal.querySelector('iframe').src = modal.querySelector('iframe').src + '?autoplay=1'
+		}
+	}
+
+	window.onclick = function(event) {  
+		if (event.target == modal) {
+			modalClose(`#${modal.id}`)
+		}
+	}
+
+	inputsClassToggle()
+}
+
+function modalClose(modalSel) {
+	let modal = document.querySelector(modalSel)
+
+	if (document.body.classList.contains('-modal-open')) {
+		document.body.classList.remove('-modal-open')
+	}
+	
+	modal.classList.remove('-is-open')
+	if (modal.querySelector('iframe')) {
+		if (modal.querySelector('iframe').src.indexOf('youtube')) {
+			modal.querySelector('iframe').src = modal.querySelector('iframe').src + '?autoplay=0'
+		}
+	}
+}
+
+function initPhoneMask() {
+	let phoneInputs = document.querySelectorAll('[type="tel"]')
+    if (phoneInputs.length) {
+        phoneInputs.forEach(phone => {
+            IMask(phone, {
+                mask: '+{7}(000)000-00-00'
+            })
+        })
+    }
+}
+
+function inputsClassToggle() {
+	let inputs = document.querySelectorAll('input[type="tel"], input[type="text"], input[type="password"], input[type="email"], textarea')
+	inputs.forEach(input => {
+		if (input.value) {
+			input.classList.add('-not-empty')
+		} else {
+			input.classList.remove('-not-empty')
+		}
+		input.addEventListener('change', function() {
+			if (this.value) {
+				this.classList.add('-not-empty')
+			} else {
+				this.classList.remove('-not-empty')
+			}
+		})
+	})
+}
